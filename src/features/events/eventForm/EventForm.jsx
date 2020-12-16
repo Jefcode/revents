20aa4579e8e20d0f63a88c2968ac1cf1daa4136a
AmirHossein JafariPanah <am.amir.husein@gmail.com>
2020-12-16 08:41:44 +0330

    End of section 5

--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,5 +1,6 @@
 import cuid from 'cuid'
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { Button, Form, Header, Segment } from 'semantic-ui-react'
 
 export default function EventForm({
@@ -108,7 +109,8 @@ export default function EventForm({
                 />
                 <Button
                     type='submit'
-                    onClick={() => setFormOpen(false)}
+                    as={Link}
+                    to='/events'
                     floated='right'
                     content='Cancel'
                 />
